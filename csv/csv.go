5efeeec4fbc7c46f23da39f35f401b60c9a65cd3
Erik Bryant <erikbryantology@gmail.com>
2020-09-12 13:20:35 -0700

    Add function to create/append records to file
    Add a function that will create a new file and write contents to it or,
    if the file already exists, can append to it.

--- csv/csv.go
+++ csv/csv.go
@@ -3,6 +3,7 @@ package csv
 import (
 	"fmt"
 	"io/ioutil"
+	"os"
 	"strings"
 )
 
@@ -22,4 +23,26 @@ func GetFile(file string) ([]string, error) {
 
 	// Skip the header line
 	return lines[1:], nil
+}
+
+// AppendFile adds to the end of a file (creates if not exist).
+func AppendFile(file, contents string, truncate bool) error {
+	mode := os.O_CREATE | os.O_WRONLY
+	if truncate {
+		mode |= os.O_TRUNC
+	} else {
+		mode |= os.O_APPEND
+	}
+	f, err := os.OpenFile(file, mode, 0644)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+
+	_, err = f.WriteString(contents)
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
