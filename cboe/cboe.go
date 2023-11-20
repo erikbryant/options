199b673a115cbee348c62dea155b1d21696f9050
Erik Bryant <erikbryantology@gmail.com>
2023-11-20 17:41:48 -0500

    Simplify, stop caching (we want this fresh)

--- cboe/cboe.go
+++ cboe/cboe.go
@@ -3,63 +3,54 @@ package cboe
 import (
 	"fmt"
 	"io"
-	"os"
-	"sort"
 	"strings"
 
-	"github.com/erikbryant/options/csv"
 	"github.com/erikbryant/web"
 )
 
-// getData downloads the CBOE weekly options data and saves it to a file.
-func getData(file string) error {
-	url := "https://www.cboe.com/us/options/symboldir/weeklys_options/?download=csv"
+// webRequest returns the downloaded payload
+func webRequest(url string) ([]byte, error) {
 	headers := map[string]string{}
 
 	resp, err := web.Request2(url, headers)
 	if err != nil {
-		return err
+		return nil, err
 	}
 
 	defer resp.Body.Close()
 
-	out, err := os.Create(file)
+	body, err := io.ReadAll(resp.Body)
 	if err != nil {
-		return fmt.Errorf("unable to create CBOE data file %v", err)
-	}
-	defer out.Close()
-
-	_, err = io.Copy(out, resp.Body)
-	if err != nil {
-		return fmt.Errorf("unable to write to CBOE data file %v", err)
+		return nil, err
 	}
 
-	return nil
+	return body, nil
 }
 
-// WeeklyOptions pulls the list of options with weekly (or more frequent) expirations from the CBOE.
+// WeeklyOptions returns options with weekly (or more frequent) expirations from the CBOE
 func WeeklyOptions() ([]string, error) {
-	file := "web-request-cache/cboesymboldirweeklys.csv"
+	url := "https://www.cboe.com/us/options/symboldir/weeklys_options/?download=csv"
 
-	err := getData(file)
+	response, err := webRequest(url)
 	if err != nil {
 		return nil, fmt.Errorf("unable to download CBOE data %v", err)
 	}
 
-	records, err := csv.GetFile(file)
-	if err != nil {
-		return nil, fmt.Errorf("unable to open CBOE data %v", err)
-	}
-
 	var tickers []string
 
-	for _, record := range records {
-		fields := strings.Split(record, "\",\"")
-		ticker := strings.Trim(fields[1], "\"")
+	rows := strings.Split(string(response), "\n")
+	for i, row := range rows {
+		if i == 0 {
+			continue
+		}
+		if row == "" {
+			continue
+		}
+		cols := strings.Split(row, ",")
+		ticker := cols[len(cols)-1]
+		ticker = strings.ReplaceAll(ticker, "\"", "")
 		tickers = append(tickers, ticker)
 	}
-
-	sort.Strings(tickers)
 
 	return tickers, nil
 }
